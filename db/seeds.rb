90107bd9d0896eb9af573d94644416a904a35cbf
Scott Chiang <scott.chiang1@gmail.com>
2017-07-27 19:59:56 -0700

    add seeds and test

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,34 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Product.create({
+  sku: "B00UV6I8QQ",
+  name: "Nikon Coolpix camera",
+  description: "Nikon Coolpix L340 20.2MP Digital Camera with 28x Optical Zoom",
+  price: "155.00"
+})
+Product.create({
+  sku: "LKASDJF8SADNFL23",
+  name: "The Fate of the Furious",
+  description: "DVD + Digital HD with Ultravio of the movie The Fate of the Furious",
+  price: "14.99"
+})
+Product.create({
+  sku: "LKJ3443FES23",
+  name: "Nintendo Switch",
+  description: "Nintendo Switch with Gray Joy-Con",
+  price: "386.91"
+})
+Product.create({
+  sku: "324235234B00U82834V6I8QQ",
+  name: "Apple iPhone 7",
+  description: "Verizon Apple iPhone 7 32GB Rose Gold",
+  price: "655.00"
+})
+Product.create({
+  sku: "U82834V6I8QQ324235",
+  name: "Coavas Backpack",
+  description: "Coavas Little Kids Backpack Dinosaur Preschool Toys Bag Best Gift for Toddlers",
+  price: "22.99"
+})
--- test/models/product_test.rb
+++ test/models/product_test.rb
@@ -1,7 +1,17 @@
 require 'test_helper'
 
 class ProductTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "create a valid custom field" do
+    product = Product.create
+    product.create_custom_field({key: 'foo', value: 'bar'})
+    assert_equal(product.custom_fields.count, 1)
+  end
+
+  test "cannot have 2 custom fields with same name" do
+    product = Product.create
+    product.create_custom_field({key: 'foo', value: 'bar'})
+    assert_raises(ActiveRecord::RecordInvalid) {
+      product.create_custom_field({key: 'foo', value: 'bazzz'})
+    }
+  end
 end
